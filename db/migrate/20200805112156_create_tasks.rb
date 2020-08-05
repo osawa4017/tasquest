376cc8faca6e69d85b3bf8a1f28f03d751d10f5f
osawa4017 <br0wn.ke1.4017@gmail.com>
2020-08-05 20:31:35 +0900

    create tasks table

---
+++ db/migrate/20200805112156_create_tasks.rb
@@ -0,0 +1,14 @@
+class CreateTasks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tasks do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :content, null: false
+      t.integer :classification, null: false
+      t.integer :point, null: false
+      t.datetime :deadline, null: false
+      t.boolean :is_complete, null: false, default: false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_04_131943) do
+ActiveRecord::Schema.define(version: 2020_08_05_112156) do
+
+  create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "content", null: false
+    t.integer "classification", null: false
+    t.integer "point", null: false
+    t.datetime "deadline", null: false
+    t.boolean "is_complete", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,4 +37,5 @@ ActiveRecord::Schema.define(version: 2020_08_04_131943) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tasks", "users"
 end
