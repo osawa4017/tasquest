e0f1a58ab1715dc27ec6694dccff8b362b3c1b11
osawa4017 <br0wn.ke1.4017@gmail.com>
2020-08-10 13:34:49 +0900

    add new action & create action to tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,5 +2,29 @@ class TasksController < ApplicationController
   before_action :authenticate_user!, only: [:index]
 
   def index
+    @routine_tasks = Task.where(classification_id: 1).order("created_at DESC").first(5)
+    @todo_tasks    = Task.where(classification_id: 2).order("created_at DESC").first(5)
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    
+    if @task.save
+      render :create
+      return
+    else
+      render :new
+      return
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:user_id, :classification_id, :content, :point, :deadline, :is_complete)
   end
 end
