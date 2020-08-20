f8fa9c64ea70a9721f007d463a046823d11b678b
osawa4017 <br0wn.ke1.4017@gmail.com>
2020-08-20 17:05:56 +0900

    remove activehash for classification

--- app/models/classification.rb
+++
@@ -1,6 +0,0 @@
-class Classification < ActiveHash::Base
-  self.data = [
-    {id: 1, name: 'Routine Task' },
-    {id: 2, name: 'ToDo Task'    },
-  ]
-end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,3 @@
 class Task < ApplicationRecord
   belongs_to :user
-
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :classification
 end
