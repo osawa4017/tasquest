837f3d08d2cc31aa3c9f86df16e0dfe9e48ea4b5
osawa4017 <br0wn.ke1.4017@gmail.com>
2020-08-24 22:16:22 +0900

    undo production settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,10 +25,6 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
-# デプロイ時に本番環境にcronの値をセット
-set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
-
-
 # デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
 set :output, 'log/crontab.log'
-set :environment, :development, :production
+set :environment, :development
 
 every 1.day, at: '5:00 am' do
   rake "create_todays_routine:todays_routine"
