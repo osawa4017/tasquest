299852b7d689196ecc20289923cd4961a315c5d8
osawa4017 <br0wn.ke1.4017@gmail.com>
2020-08-27 11:54:05 +0900

    remove silent mode

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,8 @@
 set :output, 'log/crontab.log'
 set :environment, :development, :production
 
+job_type :rake, 'cd :path && :environment_variable=:environment bundle exec rake :task :output'
+
 every 1.day, at: '5:00 am' do
   rake "create_todays_routine:todays_routine"
 end
